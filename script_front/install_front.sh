a45dcad4d00deda08ab25c0caefa4dace47e1681
Maxime Bouchet <maxime.bouchet@epitech.eu>
2020-03-19 22:51:47 +0100

    [ADD] Dockerize angular and aking scripts for easy servers starting

--- script_front/install_front.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-projectName="angClient"
-
-docker run -it --rm -w /app -v $(pwd):/app alexsuch/angular-cli:7.3.8-chromium ng new ${projectName}
-
-echo "Component ${projectName} created!"
