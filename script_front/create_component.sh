a8147efbbd948b24dce186bc552999d260ff5a41
arthur baron <baronarthur3@gmail.com>
2020-03-20 18:47:36 +0100

    [MERGE] merge done.

--- script_front/create_component.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Enter component name : "
-
-read component
-
-docker run -it --rm -w /app -v $(pwd)/angClient:/app alexsuch/angular-cli:7.3.8-chromium ng g component component
-
-echo "Component ${comp} created!"
