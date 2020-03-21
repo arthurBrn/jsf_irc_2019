f67277a4d3289c505d01d2154d6ae334434f94b0
Maxime Bouchet <maxime.bouchet@epitech.eu>
2020-03-21 18:39:54 +0100

    [ADD] Connection between angular client and socket server

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,11 @@ const io = require('socket.io').listen(server)
 const HOST = 8080;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.send('Socket server on !');
 })
 
 io.sockets.on('connection', (socket, pseudo) => {
+  console.log('new client connected');
   socket.on('new_member', (pseudo) => {
     socket.pseudo = pseudo;
     socket.broadcast.emit('new_member', socket.pseudo);
