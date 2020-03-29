b909cda9cbb4587899e207ee2b5c1304ebef7243
Maxime Bouchet <maxime.bouchet@epitech.eu>
2020-03-29 17:51:43 +0200

    [ADD] User password encryption in database

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -4,14 +4,7 @@ var Connection = {
   login: (datas, callback) => {
     return db.query('SELECT * FROM users WHERE email = ?', [datas.email], callback);
   },
-  register: (datas, callback) => {
-    var user = {
-    first_name: datas.first_name,
-    last_name: datas.last_name,
-    email: datas.email,
-    password: datas.password,
-    createdAt: datas.createdAt
-  }
+  register: (user, callback) => {
     return db.query('INSERT INTO users SET ?', user, callback);
   }
 }
--- backend/routes/loginController.js
+++ backend/routes/loginController.js
@@ -3,24 +3,26 @@ var router = express.Router()
 var bodyParser = require('body-parser')
 router.use(bodyParser.json())
 var Connection = require('./login')
-
+var bcrypt = require('bcrypt')
 router.post('/login', (req, res) => {
   Connection.login(req.body, (err, rows) => {
     if (err) {
       res.status(400).json(err)
     } else {
       if (rows.length > 0) {
-        if (rows[0].password == req.body.password) {
-          res.json({
-            code: 200,
-            success: 'login sucessfull'
-          })
-        } else {
-          res.json({
-            code: 204,
-            success: 'Email and password does not match'
-          })
-        }
+        bcrypt.compare(req.body.password, rows[0].password, (err, result) => {
+          if (result) {
+            res.json({
+              code: 200,
+              success: 'login sucessfull'
+            })
+          } else {
+            res.json({
+              code: 204,
+              success: 'Email and password does not match'
+            })
+          }
+        })
       } else {
         res.json({
           code: 204,
@@ -32,18 +34,29 @@ router.post('/login', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  Connection.register(req.body, (error, rows) => {
-    if (error) {
-      res.send({
-        code: 400,
-        failed: 'error ocurred'
-      })
-    } else {
-      res.send({
-        code: 200,
-        success: 'user registered sucessfully'
-      })
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+    var user = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      password: hash,
+      createdAt: req.body.createdAt
     }
+    console.log(user);
+    Connection.register(user, (error, rows) => {
+      if (error) {
+        console.log(error);
+        res.send({
+          code: 400,
+          failed: 'error ocurred'
+        })
+      } else {
+        res.send({
+          code: 200,
+          success: 'user registered sucessfully'
+        })
+      }
+    })
   })
 })
 
