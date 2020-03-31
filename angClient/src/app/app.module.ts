773bace0ee80d9ee603d7763de32fb9108929837
arthur baron <baronarthur3@gmail.com>
2020-03-31 21:54:27 +0200

    [EDIT] user-channel component added.

--- angClient/src/app/app.module.ts
+++ angClient/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule ,ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AutofocusModule } from 'angular-autofocus-fix';
