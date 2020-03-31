d07d99cef92975c3e71fe5deb2f13eb928c87402
arthur baron <baronarthur3@gmail.com>
2020-03-31 23:46:22 +0200

    [EDIT] recover message sent from user.

---
+++ angClient/src/app/chat/chat.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit {
+
+  @Input() messages;
+  @Input() message;
+  @Input() function: any;
+  @Output() emittedValue = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSendMessage() {
+    this.emittedValue.emit(this.message);
+  }
+}
